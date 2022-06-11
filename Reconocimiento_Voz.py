ba3c12e8237ec5961e7761add0fd9dda2342d812
Raigoza <neggerchip@gmail.com>
2022-06-10 23:00:36 -0500

    Version 2 funcional

--- Reconocimiento_Voz.py
+++ Reconocimiento_Voz.py
@@ -2,14 +2,19 @@
 
 import speech_recognition as sr
 import pyaudio as audio
+import pprint as pp
 
 receptor = sr.Recognizer();
-
+receptor.dynamic_energy_threshold = True;
+receptor.pause_threshold = 0.5;
 
 
 try:
-    with sr.Microphone() as source:
-        print("Escuchando");
+    with sr.Microphone() as source: # 6
+        receptor.adjust_for_ambient_noise(source, duration=1);
+        #pp.pprint(source.get_pyaudio())
+        print(receptor.energy_threshold);
+        print("Escuchando...");
         voice = receptor.listen(source);
         rec = receptor.recognize_google(voice);
         print(rec);
