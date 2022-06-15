0f34556a8f4b6e021fe6604a22e5ebe0621b4f1e
Raigoza <neggerchip@gmail.com>
2022-06-14 21:15:25 -0500

    Añado comentarios y en la main, agrego que repida lo que se dice

--- Reconocimiento_Voz.py
+++ Reconocimiento_Voz.py
@@ -6,25 +6,22 @@ import pprint as pp
 from testToSpeech import *
 
 talker = Talker();
-
 talker.setVoice(voice=3);
 
-talker.talk(text="Mi casa es de color verde");
 
-talker.showAllVoices();
+receptor = sr.Recognizer(); # Este objeto tiene todo lo importante con el reconocimiento
+receptor.dynamic_energy_threshold = True; # Para noise dinamico
+receptor.pause_threshold = 0.5; # Tiempo en segundos que dura en terminar un audio.
 
-# receptor = sr.Recognizer(); # Este objeto tiene todo lo importante con el reconocimiento
-# receptor.dynamic_energy_threshold = True; # Para noise dinamico
-# receptor.pause_threshold = 0.5; # Tiempo en segundos que dura en terminar un audio.
-
-# try:
-#     with sr.Microphone() as source: # 6
-#         receptor.adjust_for_ambient_noise(source, duration=1);
-#         #pp.pprint(source.get_pyaudio())
-#         print(receptor.energy_threshold);
-#         print("Escuchando...");
-#         voice = receptor.listen(source);
-#         rec = receptor.recognize_google(voice,language="es-CO");
-#         print(rec);
-# except:
-#     pass
+try:
+    with sr.Microphone() as source: # 6
+        receptor.adjust_for_ambient_noise(source, duration=1);
+        #pp.pprint(source.get_pyaudio())
+        print(receptor.energy_threshold);
+        print("Escuchando...");
+        voice = receptor.listen(source);
+        rec = receptor.recognize_google(voice,language="es-CO");
+        print(rec);
+        talker.talk(text=rec);
+except:
+    pass
